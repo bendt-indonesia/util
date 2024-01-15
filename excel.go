fd8984b71abf902a417c840c37f2798e68e9dd98
Bendt Indonesia <46597618+bendt-indonesia@users.noreply.github.com>
2024-01-15 10:31:09 +0700

    Add ReadExcelFile & ReadSheets

---
+++ excel.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"fmt"
+)
+
+func ReadExcelFile(path string) (*excelize.File, error) {
+	f, err := excelize.OpenFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	return f, nil
+}
+
+func ReadSheets(f *excelize.File) []string {
+	var sheets []string
+	if f == nil {
+		return sheets
+	}
+
+	for _, name := range f.GetSheetMap() {
+		sheets = append(sheets, name)
+	}
+
+	return sheets
+}
