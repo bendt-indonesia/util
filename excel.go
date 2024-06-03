8a41143e7b86c4dd3e77a29982eb38cfde3f3d11
Bendt Indonesia <46597618+bendt-indonesia@users.noreply.github.com>
2024-06-03 15:01:28 +0700

    Excel func

--- excel.go
+++ excel.go
@@ -33,3 +33,30 @@ func ReadSheets(f *excelize.File) []string {
 
 	return sheets
 }
+
+func CreateEmptyExcelFile() *excelize.File {
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println("[CreateEmptyExcelFile]", err)
+		}
+	}()
+
+	return f
+}
+
+func CreateNewSheet(f *excelize.File, sheetName string) (*excelize.File, int, error) {
+	index, err := f.NewSheet(sheetName)
+	if err != nil {
+		return nil, -1, err
+	}
+	return f, index, nil
+}
+
+func SaveExcelAs(f *excelize.File, saveAsName string) error {
+	if err := f.SaveAs(saveAsName); err != nil {
+		return err
+	}
+
+	return nil
+}
